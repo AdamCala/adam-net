ec34625b5509a95748560e279f4ff3c7fc831880
Adam Cała <adam.cala31@gmail.com>
2023-11-16 21:31:01 +0100

    added grain filter and fixed scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
 import React from "react";
-import "@/styles/index.module.scss";
+import styles from "@/styles/index.module.scss";
+import { Fascinate_Inline } from "next/font/google";
+
+const fascinate_inline = Fascinate_Inline({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 const page = () => {
-  return <div className="main"></div>;
+  return (
+    <div className="main">
+      <div
+        className={`${fascinate_inline.className} ${styles.flagAdamContainer}`}
+      >
+        <p className={`${styles.flagAdamFirst} `}>Adam Cala</p>
+        <p className={`${styles.flagAdamSecond} `}>Adam Cala</p>
+      </div>
+    </div>
+  );
 };
 
 export default page;
