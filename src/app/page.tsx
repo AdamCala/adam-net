833859b90e5df4208243af475a64aa8d8db20ea9
Adam Cała <adam.cala31@gmail.com>
2023-11-16 23:13:19 +0100

    added responsive design and more text to landing page also remembered that text shadow exisits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "@/styles/index.module.scss";
 import { Fascinate_Inline } from "next/font/google";
+import { Courier_Prime } from "next/font/google";
 
 const fascinate_inline = Fascinate_Inline({
+  subsets: ["latin"],
+  weight: "400",
+});
+const courier_prime = Courier_Prime({
   subsets: ["latin"],
   weight: "400",
 });
@@ -13,8 +18,13 @@ const page = () => {
       <div
         className={`${fascinate_inline.className} ${styles.flagAdamContainer}`}
       >
-        <p className={`${styles.flagAdamFirst} `}>Adam Cala</p>
-        <p className={`${styles.flagAdamSecond} `}>Adam Cala</p>
+        <div
+          className={`${courier_prime.className} ${styles.subtextAdamContainer}`}
+        >
+          <p className={`${styles.subtextAdam}`}>I write code</p>
+          <p className={`${styles.subtextAdam}`}>on the internet</p>
+        </div>
+        <p className={`${styles.flagAdam} `}>Adam Cala</p>
       </div>
     </div>
   );
